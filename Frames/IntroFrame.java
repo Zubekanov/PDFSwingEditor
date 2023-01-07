742beb79eb6dabc62dc0802eb73c39fdeca4d207
Joseph Wong <josephwong17@gmail.com>
2023-01-07 17:24:24 +1100

    Initial file commit

---
+++ Frames/IntroFrame.java
@@ -0,0 +1,8 @@
+package PDFSwingEditor.Frames;
+import javax.swing.*;
+
+public class IntroFrame extends JFrame {
+    public IntroFrame() {
+        
+    }
+}
---
+++ MainHandler.java
@@ -0,0 +1,10 @@
+package PDFSwingEditor;
+
+import PDFSwingEditor.Frames.*;
+import PDFSwingEditor.Parser.*;
+
+public class MainHandler {
+    public static void main(String[] args) {
+        new IntroFrame();
+    }
+}
